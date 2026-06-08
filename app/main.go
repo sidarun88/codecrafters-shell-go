bc9d3758bbeaea08d3a58d7735f91a8c1240d0d4
Sidharth Arun <sidarun88@gmail.com>
2026-06-08 22:08:59 +0530

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -5,5 +5,11 @@ import (
 )
 
 func main() {
+	var cmd string
 	fmt.Print("$ ")
+	_, err := fmt.Scanln(&cmd)
+	if err != nil {
+		fmt.Println("Error:", err)
+	}
+	fmt.Printf("%s: command not found\n", cmd)
 }

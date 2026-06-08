e3b85d6c1edb42240498d177f486db71e36f3f74
Sidharth Arun <sidarun88@gmail.com>
2026-06-08 23:58:59 +0530

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -4,17 +4,25 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
 
 func main() {
 	reader := bufio.NewReader(os.Stdin)
 	for {
 		fmt.Print("$ ")
+
 		cmd, err := reader.ReadString('\n')
 		if err != nil {
 			fmt.Println("Error:", err)
 			os.Exit(1)
 		}
-		fmt.Printf("%s: command not found\n", cmd[:len(cmd)-1])
+
+		cmd = strings.TrimSpace(cmd)
+		if cmd == "exit" {
+			os.Exit(0)
+		}
+
+		fmt.Printf("%s: command not found\n", cmd)
 	}
 }
